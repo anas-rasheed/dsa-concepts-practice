856016c1d22b97adc1101735abcca5f4f090a6ad
Anas Rasheed <76838052+anas-rasheed@users.noreply.github.com>
2021-07-06 02:09:40 +0530

    added binary search implementation

---
+++ search-algorithms/binary-search.js
@@ -0,0 +1,31 @@
+/**
+ * Binary Search Implementation
+ *
+ * Searches the input searchItem from a sorted array
+ * @param {number[]} inputArray Sorted input array
+ * @param {number} searchItem Number to be searched
+ * @returns {number} Index of the searchItem in inputArray
+ */
+
+const binarySearch = (inputArray, searchItem) => {
+  let low = 0;
+  let high = inputArray.length - 1;
+  while (low <= high) {
+    let mid = Math.round((low + high) / 2);
+    let midTerm = inputArray[mid];
+    if (midTerm === searchItem) {
+      return mid;
+    }
+    if (midTerm < searchItem) {
+      low = mid + 1;
+    } else if (midTerm > searchItem) {
+      high = mid - 1;
+    }
+  }
+};
+
+const testArray1 = [2, 4, 6, 8, 9, 10];
+const testArray2 = [11, 22, 31, 35, 36, 73];
+
+console.log(binarySearch(testArray1, 6));
+console.log(binarySearch(testArray2, 36));
